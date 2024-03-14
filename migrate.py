0ea819b70e0494c0926f33bba490330149e7da10
Amandus Butzer <amandus.butzer@heigit.org>
2024-03-14 15:39:18 +0100

    feat: add migration & validation functions
    - move from main to migrate function
    - add validation for input and output files

--- migrate.py
+++ migrate.py
@@ -4,6 +4,11 @@ import sys
 
 from os.path import join, dirname
 
+from pydantic import ValidationError
+
+from models.yml_config import OrsConfigYML
+from models.json_config import OrsConfigJSON, Parameters, ProfileEntry
+
 
 def get_recursive(d, dot_string, remove=False):
     keys = dot_string.split('.')
@@ -58,6 +63,72 @@ def remove_and_output(jsondict, jsonpath, msg=''):
         print(f"Info: No property for '{jsonpath}' to remove.")
 
 
+def validate_profiles(json_dict, error_list):
+    profiles = json_dict['ors']['services']['routing']['profiles']
+    for key, value in profiles.items():
+        if key == "active":
+            try:
+                assert isinstance(value, list)
+                for entry in value:
+                    assert isinstance(entry, str)
+            except AssertionError as err:
+                print(f"Wrong type for 'ors.services.routing.profiles.active'")
+                error_list.append(e)
+                print()
+        if key == "default_params":
+            try:
+                Parameters.model_validate(value)
+            except ValidationError as e:
+                format_e = '\n'.join(str(e).split('\n')[:2])
+                print(
+                    f"Unknown config property found in 'ors.services.routing.profiles.default_parameters': {format_e}")
+                error_list.append(e)
+                print()
+        if key.startswith("profile-"):
+            try:
+                ProfileEntry.model_validate(value)
+            except ValidationError as e:
+                format_e = '\n'.join(str(e).split('\n')[:2])
+                print(f"Unknown config property found in 'ors.services.routing.profiles.{key}': {format_e}")
+                error_list.append(e)
+                print()
+
+
+def migrate(json_config_path, yaml_config_path):
+    """
+    Program to migrate an existing ors-config.json to the new ors-config.yml format.
+    Includes validation of config input and reports on invalid or non-transferable entries.
+    For further information and configuration of ORS via environment variables
+    see https://giscience.github.io/openrouteservice/run-instance/configuration
+    """
+    results = {
+        "actions_needed": [],
+        "info": [],
+        "warnings": [],
+        "errors": []
+    }
+    with open(join(dirname(__file__), str(json_config_path)), 'r') as f:
+        x = json.load(f)
+
+    try:
+        OrsConfigJSON.model_validate(x)
+    except ValidationError as e:
+        results['errors'].append(e)
+
+    validate_profiles(x, results["errors"])
+
+    print(f'Migrating file from {json_config_path} to {yaml_config_path}')
+
+    try:
+        OrsConfigYML.model_validate(x)
+    except ValidationError as e:
+        raise e
+
+    with open(yaml_config_path, 'w') as f:
+        f.writelines(yaml.dump(x))
+        print(f'Wrote yml output to {f.name}')
+
+
 if __name__ == "__main__":
     args = sys.argv[1:]
     in_file = args[0]
@@ -69,11 +140,4 @@ if __name__ == "__main__":
         print("Usage: python migrate.py ./your-ors-config.json [./ors-config.yml]")
     elif len(args) == 2:
         out_file = args[1]
-
-    print(f'Migrating file from {in_file} to {out_file}')
-
-    x = json.load(open(join(dirname(__file__), str(in_file))))
-
-    with open(out_file, 'w') as f:
-        f.writelines(yaml.dump(x))
-        print(f'Wrote yml output to {f.name}')
+    migrate(in_file, "./ors-config.yml")
