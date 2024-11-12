c13ed432e78b27e76f203273e1ab5a6405f555e6
merydian <till.frankenbach@gmail.com>
2024-11-12 15:42:27 +0100

    test: add test for migrate_8_9()

--- tests/test_migrate.py
+++ tests/test_migrate.py
@@ -3,7 +3,7 @@ import shutil
 from os.path import join, dirname
 from unittest import TestCase
 
-from migrate import migrate_7_to_8, if_exists_move_to
+from migrate import migrate_7_to_8, if_exists_move_to, migrate_8_to_9
 
 
 class Test(TestCase):
@@ -13,20 +13,20 @@ class Test(TestCase):
             shutil.rmtree('./outputs')
         os.mkdir('./outputs')
 
-    def test_migrate_sample_config(self):
+    def test_migrate_sample_config_7_8(self):
         in_file = '../config-files-json/ors-config-sample.json'
 
-        migrate_7_to_8(join(dirname(__file__), in_file), "./outputs/ors-config-sample-migrated.yml")
+        migrate_7_to_8(join(dirname(__file__), in_file), "./outputs/ors-config-sample-7-8-migrated.yml")
 
-    def test_migrate_test_config(self):
+    def test_migrate_test_config_7_8(self):
         in_file = '../config-files-json/test-config.json'
 
-        migrate_7_to_8(join(dirname(__file__), in_file), "./outputs/test-config-migrated.yml")
+        migrate_7_to_8(join(dirname(__file__), in_file), "./outputs/test-config-7-8-migrated.yml")
 
-    def test_migrate_production_config(self):
+    def test_migrate_production_config_7_8(self):
         in_file = '../config-files-json/ors-config.driving-car.json'
 
-        migrate_7_to_8(join(dirname(__file__), in_file), "./outputs/ors-config.driving-car-migrated.yml")
+        migrate_7_to_8(join(dirname(__file__), in_file), "./outputs/ors-config.driving-car-7-8-migrated.yml")
 
     def test_if_exists_move_to(self):
         test_dict = {"info": {
@@ -52,3 +52,8 @@ class Test(TestCase):
         if_exists_move_to(test_dict, 'info.1.2.3', '4')
         self.assertNotIn('1', test_dict.get('info'), "Empty Origin kept.")
         self.assertIn('4', test_dict, "Not moved to location")
+
+    def test_migrate_sample_config_8_9(self):
+        in_file = '../config-files-yaml/ors-config-source-8.yml'
+
+        migrate_8_to_9(join(dirname(__file__), in_file), "./outputs/ors-config-sample-8-9-migrated.yml")
